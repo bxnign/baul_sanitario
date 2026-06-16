17c7b23eba23a5db5be97f0ec460d89ac9c8bc3c
sinqroo <benjaignacioiv@gmail.com>
2026-06-16 14:53:21 -0400

    Agregar tipo de documento 'MEDICAL_RECORD' en la enumeración DocumentType

--- app/src/main/java/com/baulsanitario/domain/model/DocumentType.kt
+++ app/src/main/java/com/baulsanitario/domain/model/DocumentType.kt
@@ -2,6 +2,7 @@ package com.baulsanitario.domain.model
 
 enum class DocumentType(val displayName: String) {
     RECIPE("Receta"),
+    MEDICAL_RECORD("Registro médico"),
     EXAM("Examen"),
     RECEIPT("Boleta"),
     ORDER("Orden médica")
